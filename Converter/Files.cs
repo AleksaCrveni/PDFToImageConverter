c39e8c0e0a82661270d5b100c0ac167652f4bb5b
WickedyWick <crvenialeksa@gmail.com>
2025-05-27 22:08:26 +0200

    Add global files paths

---
+++ Converter/Files.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+  public static class Files
+  {
+    public static string RootFolder { get; set; }
+    public static string BaseDocFilePath { get; set; }
+    public static string F2_0_2020 { get; set; }
+    public static string Report { get; set; }
+    static Files()
+    { 
+      RootFolder = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Files");
+      BaseDocFilePath = Path.Combine(RootFolder, "BaseDoc.pdf");
+      F2_0_2020 = Path.Combine(RootFolder, "PDF2.0.2020.pdf");
+      Report = Path.Combine(RootFolder, "Report.pdf");
+
+    }
+  }
+}
