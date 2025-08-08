e6295c20368cc7ccdf14fdb785d1ed268fc6dc5b
WickedyWick <crvenialeksa@gmail.com>
2025-08-08 19:40:00 +0200

    Small test proj

---
+++ BenchAndSmallTests/Program.cs
@@ -0,0 +1,8 @@
+﻿using Converter;
+
+Stream stream = File.OpenRead(Files.SmallTest);
+
+Span<byte> buffer = stackalloc byte[1024];
+int bytesRead = stream.Read(buffer);
+
+int i = 0;
--- Converter/Files.cs
+++ Converter/Files.cs
@@ -6,13 +6,14 @@
     public static string BaseDocFilePath { get; set; }
     public static string F2_0_2020 { get; set; }
     public static string Report { get; set; }
+    public static string SmallTest { get; set; }
     static Files()
     { 
       RootFolder = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Files");
       BaseDocFilePath = Path.Combine(RootFolder, "BaseDoc.pdf");
       F2_0_2020 = Path.Combine(RootFolder, "PDF2.0.2020.pdf");
       Report = Path.Combine(RootFolder, "Report.pdf");
-
+      SmallTest = Path.Combine(RootFolder, "SmallTest.txt");
     }
   }
 }
