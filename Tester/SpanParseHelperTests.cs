fd607d1c665c559c9676f9566f9cf0e1f32b0774
WickedyWick <crvenialeksa@gmail.com>
2025-08-16 12:51:38 +0200

    Token stests

---
+++ Tester/SpanParseHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+  [TestClass]
+  public sealed class SpanParseHelperTests
+  {
+    public SpanParseHelperTests()
+    {
+
+    }
+
+    [TestMethod]
+    public void GetNextTokenTest()
+    { 
+    
+    }
+  }
+}
