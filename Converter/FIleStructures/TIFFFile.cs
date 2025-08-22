49988af2871eda8c6d0be159ed3f51bb02c0973b
WickedyWick <crvenialeksa@gmail.com>
2025-08-22 19:56:27 +0200

    tag support

--- Converter/FIleStructures/TIFFFile.cs
+++ Converter/FIleStructures/TIFFFile.cs
@@ -6,7 +6,8 @@
     public TIFFHeader Header = new TIFFHeader();
     public Stream Stream { get; set; }
     // this isn't global since there are more lists, but for now we just parse 1 so keep
-    public Tags Tags { get; set; }
+    public List<Tag> Tags { get; set; } = new List<Tag>();
+    public int TotalPages = -1; // -1 in case its not available
   }
 
   public struct TIFFHeader
@@ -19,32 +20,65 @@
   // 12 B
   public struct IFD
   {
-    public short Tag;
-    public short Type;
-    public int Count;
-    public int ValueOrOffset;
+    public ushort Tag;
+    public ushort Type;
+    public uint Count;
+    public uint ValueOrOffset;
   }
 
   // Maybe in future have one big struct
   // there are ~90 tags?
   // 32 * 90
   // just do it for now and fix if any issues
-  public struct Tags
+  // there is a problem with default values because 0 is taken
+  // so I am just going to make field nullable and check for that.
+  public struct Tag
   {
-    public short PhotometricInterpretation;
-    public short Compression;
-    public int ImageLength;
-    public int ImageWidth;
-    public short ResolutionUnit;
-    // in actual data this is represented as 2 ints, 1 for numerator and one for denominator
+    public Tag() { }
+    public PhotometricInterpretation? PhotometricInterpretation;
+    public Compression? Compression = FIleStructures.Compression.NoCompression;
+    public ushort? BitsPerSample;
+    public uint? ImageLength;
+    public uint? ImageWidth;
+    public ResolutionUnit ResolutionUnit = ResolutionUnit.Inch;
+    // in actual data this is represented as 2 uints, 1 for numerator and one for denominator
     // but we will process it directly in double
-    public double XResolution;
+    public double? XResolution;
     // -||-
-    public double YResolution;
-    public int RowsPerStrip;
-    public int StripOffsets;
-    public int StripByteCounts;
-    public int NewSubfileType;
-    public short BitsPerSample;
+    public double? YResolution;
+    // StripsPerImage = floor ((ImageLength + RowsPerStrip - 1) / RowsPerStrip - use for
+    public uint? RowsPerStrip;
+    public uint? StripOffsets;
+    // required
+    public uint? StripByteCounts;
+    public uint NewSubfileType = 0;
+    public ushort FillOrder = 1;
+    public ushort Orientation = 1;
+    public ushort SamplesPerPixel = 1;
+    public ushort PlanarConfiguration = 1;
+    public ushort PageNumber;
   }
+
+  public enum PhotometricInterpretation
+  {
+    WhiteIsZero,
+    BlackIsZero,
+    RGB,
+    Pallete,
+    TransparencyMask
+  }
+  public enum Compression
+  {
+    NoCompression = 1,
+    CCITT = 2,
+    PackBits = 32773
+  }
+
+  public enum ResolutionUnit
+  {
+    NoAbsoluteUnitOfMe = 1,
+    Inch = 2,
+    Centimeter = 3
+  }
+
 }
