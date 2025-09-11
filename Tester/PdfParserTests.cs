aa1f72a33de6971e72da43bb3e88df74de2874cd
WickedyWick <crvenialeksa@gmail.com>
2025-09-11 21:08:19 +0200

    Added more asserts for base file

--- Tester/PdfParserTests.cs
+++ Tester/PdfParserTests.cs
@@ -13,10 +13,10 @@ namespace Tester
     }
 
     [TestMethod]
-    public void BasicTrailerSucess()
+    public void BaseDocTest()
     {
       PdfParser pdfParser = new PdfParser();
-      PDFFile pdfFile = pdfParser.Parse(Files.Sample);
+      PDFFile pdfFile = pdfParser.Parse(Files.BaseDocFilePath);
 
       Assert.IsTrue(pdfFile.PdfVersion == PDFVersion.V1_3);
       Assert.IsTrue(pdfFile.LastCrossReferenceOffset == 10777);
@@ -27,6 +27,7 @@ namespace Tester
       Assert.IsTrue(pdfFile.Trailer.ID[1] == "7d7c94e7ba72081782315eefabec9c1d");
       Assert.IsTrue(pdfFile.Trailer.EncryptIR == default((int, int)));
       Assert.IsTrue(pdfFile.Trailer.XrefStm == default(int));
+
 
       var cRefTableEntries = pdfFile.CrossReferenceEntries;
       Assert.IsTrue(cRefTableEntries.Count == pdfFile.Trailer.Size);
@@ -90,6 +91,63 @@ namespace Tester
 
       Assert.IsTrue(pdfFile.PageInformation[0].ContentDict.Length == 166);
       Assert.IsTrue(pdfFile.PageInformation[0].ContentDict.Filters[0] == Filter.FlateDecode);
+
+      ResourceDict resourceDict = pdfFile.PageInformation[0].ResourceDict;
+      FontInfo tt2FI = resourceDict.Font["TT2"];
+      
+      Assert.IsTrue(tt2FI.SubType == FontType.TrueType);
+      Assert.IsTrue(tt2FI.BaseFont == "QVIYZW+AvenirNext-Regular");
+      Assert.IsTrue(tt2FI.Encoding == EncodingInf.MacRomanEncoding);
+      Assert.IsTrue(tt2FI.FirstChar == 32);
+      Assert.IsTrue(tt2FI.LastChar == 117);
+      int[] tt2FIWidths = new int[117 - 32 + 1]
+      {
+        250,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 260, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 757, 0, 562, 0, 0, 0, 492, 0, 0, 0, 0, 0, 580, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 534, 0, 0, 0, 572, 0, 0, 0, 250, 0, 0, 252, 883, 0, 0, 635, 0, 0, 444, 317, 581
+      };
+      for (int i = 0; i < tt2FI.Widths.Length; i++)
+      {
+        if (tt2FI.Widths[i] != tt2FIWidths[i])
+          throw new InvalidDataException("Invalid width array!");
+      }
+      
+      FontDescriptor tt2FD = tt2FI.FontDescriptor;
+
+      Rect tt2FontBBox = new Rect();
+      tt2FontBBox.FillRect(-394, -411, 1309, 1192);
+
+      Assert.IsTrue(tt2FD.FontName == "QVIYZW+AvenirNext-Regular");
+      Assert.IsTrue((tt2FD.Flags & FontFlags.Nonsymbolic) == FontFlags.Nonsymbolic);
+      Assert.IsTrue(tt2FD.FontBBox == tt2FontBBox);
+      Assert.IsTrue(tt2FD.ItalicAngle == 0);
+      Assert.IsTrue(tt2FD.Ascent == 1000);
+      Assert.IsTrue(tt2FD.Descent == -366);
+      Assert.IsTrue(tt2FD.CapHeight == 708);
+      Assert.IsTrue(tt2FD.StemV == 72);
+      Assert.IsTrue(tt2FD.XHeight == 468);
+      Assert.IsTrue(tt2FD.StemH == 64);
+      Assert.IsTrue(tt2FD.MaxWidth == 1569);
+      Assert.IsTrue(tt2FD.AvgWidth == 455);
+
+      FontFileInfo tt2FontFileInfo = tt2FI.FontDescriptor.FontFile;
+      Assert.IsTrue(tt2FontFileInfo.CommonStreamInfo.Length == 3343);
+      Assert.IsTrue(tt2FontFileInfo.CommonStreamInfo.Filters[0] == Filter.FlateDecode);
+
+      // no idea how to test actual value of decoded thing because its just random bytes and can't be easily
+      // copy pasted somewhere
+      Assert.IsTrue(tt2FontFileInfo.Length1 == 5628);
+      Assert.IsTrue(tt2FontFileInfo.Length2 == 0);
+      Assert.IsTrue(tt2FontFileInfo.Length3 == 0);
+
+
     }
+
+    [TestMethod]
+    public void SampleDocTest()
+    {
+      PdfParser pdfParser = new PdfParser();
+      PDFFile pdfFile = pdfParser.Parse(Files.Sample);
+      Assert.IsTrue(1 == 1);
+    }
+
   }
 }
