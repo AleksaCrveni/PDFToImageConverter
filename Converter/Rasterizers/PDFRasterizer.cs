ba86bbc8db967ae9a1d785ae1a6b918fc7a4193c
WickedyWick <crvenialeksa@gmail.com>
2025-10-16 21:04:36 +0200

    Add PDF specific rasterizer

---
+++ Converter/Rasterizers/PDFRasterizer.cs
@@ -0,0 +1,10 @@
+﻿namespace Converter.Rasterizers
+{
+  /// <summary>
+  /// PDF Specific rasterizer, it builds on top of STBTrueType to address PDF specific things, while keeping STBTrueType.cs funtionality same
+  /// as stb_truetype.h
+  /// </summary>
+  public class PDFRasterizer
+  {
+  }
+}
