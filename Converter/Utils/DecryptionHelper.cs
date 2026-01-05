69a40151f7db1e3b801cc06583dd8ebaa7939ba4
WickedyWick <crvenialeksa@gmail.com>
2026-01-05 23:15:35 +0100

    Decryption helper fixes

--- Converter/Utils/DecryptionHelper.cs
+++ Converter/Utils/DecryptionHelper.cs
@@ -16,7 +16,7 @@ namespace Converter.Utils
     }
     public static byte[] DecryptAdobeType1EEXEC(ReadOnlySpan<byte> encrypted)
     {
-      return DecryptAdobeType1Encryption(encrypted, EEXEC_CONST_PWD, 0);
+      return DecryptAdobeType1Encryption(encrypted, EEXEC_CONST_PWD, 4);
     }
     // this is 1 to 1 decryption/encryotion just need to ac count for lenIV
     // This can be calculated using Lenght2 and 1 so we dont have to read each character but can slice directly in the parent
@@ -31,12 +31,13 @@ namespace Converter.Utils
       byte[] decrypted = new byte[encrypted.Length - lenIV];
       for (int i = 0; i < encrypted.Length; i++)
       {
-        // eh , I really need to check if comp will expand to int automatically, but i need to make sure they wrap, so do this for now..
-        r = (ushort)((ushort)((ushort)(encrypted[i] + r) * c1) + c2);
         // In case where lenIV is not 0 we have stil have to calculate r but we won't make it part of decrypted bytes because they are random fillers
         // Instead of having if here we can just split it in 2 loops but w/e
         if (i >= lenIV)
           decrypted[i - lenIV] = (byte)(encrypted[i] ^ (r >> 8));
+
+        // eh , I really need to check if comp will expand to int automatically, but i need to make sure they wrap, so do this for now..
+        r = (ushort)((ushort)((ushort)(encrypted[i] + r) * c1) + c2);
       }
 
       return decrypted;
