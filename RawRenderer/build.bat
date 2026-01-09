31957865f4c353d4dc6bfd30dfb06e5b15974121
WickedyWick <crvenialeksa@gmail.com>
2026-01-09 19:11:54 +0100

    Use ifstream instead of fread and add some perf tracking

--- RawRenderer/build.bat
+++ RawRenderer/build.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir builds
 pushd builds
-cl -FC -Zi ..\main.cpp user32.lib Gdi32.lib
+cl -FC -Zi ..\main.cpp user32.lib Gdi32.lib /EHsc
 popd
--- RawRenderer/main.cpp
+++ RawRenderer/main.cpp
@@ -7,10 +7,27 @@
 #include <thread>
 
 using namespace std;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef int32 bool32;
+
 typedef uint8_t uint8;
+typedef uint16_t uint16;
 typedef uint32_t uint32;
-#define WIDTH 800
-#define HEIGHT 600
+typedef uint64_t uint64;
+
+typedef float real32;
+typedef double real64;
+
+#define global_var static
+
+global_var int RENDER_WIDTH = 25;
+global_var int RENDER_HEIGHT = 25;
+global_var int WINDOW_WIDTH = 500;
+global_var int WINDOW_HEIGHT = 500;
 
 const TCHAR* dir = _T("..\\..\\RasterPlayground\\bin\\Debug\\net8.0\\Single\\");
 const char* filePath = "..\\..\\RasterPlayground\\bin\\Debug\\net8.0\\Single\\data.txt";
@@ -70,9 +87,9 @@ LRESULT CALLBACK Win32MainWindowCallback(
 			int Y = Paint.rcPaint.top;
 			int Height = Paint.rcPaint.bottom  - Paint.rcPaint.top;
 			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-
+			win32_window_dimension Dimension = Win32GetWindowDimension(Window);
 			Win32CopyBufferToWindow(
-				DeviceContext, WIDTH, HEIGHT,
+				DeviceContext, Dimension.Width, Dimension.Width,
 				&GlobalBuffer);
 			EndPaint(Window, &Paint);
 			
@@ -93,14 +110,19 @@ LRESULT CALLBACK Win32MainWindowCallback(
 int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, int ShowCode)
 {  
   thread t(Observe);
+
+	LARGE_INTEGER PerfCounterFreqResult;
+	QueryPerformanceFrequency(&PerfCounterFreqResult);
+	int64 PerfCounterFreq = PerfCounterFreqResult.QuadPart;
+
   GlobalBuffer = {};
   // 4 is for bytes per pixel
-  GlobalBuffer.Memory = VirtualAlloc(0, WIDTH*HEIGHT*4, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+  GlobalBuffer.Memory = VirtualAlloc(0, RENDER_WIDTH*RENDER_HEIGHT*4, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
   GlobalBuffer.Info = {};
   GlobalBuffer.Info.bmiHeader.biSize = sizeof(GlobalBuffer.Info.bmiHeader);
-	GlobalBuffer.Info.bmiHeader.biWidth = WIDTH;
+	GlobalBuffer.Info.bmiHeader.biWidth = RENDER_WIDTH;
 	// negative so bitmap is top to btottom and origin is upper left corner
-	GlobalBuffer.Info.bmiHeader.biHeight = -HEIGHT;
+	GlobalBuffer.Info.bmiHeader.biHeight = -RENDER_HEIGHT;
 	GlobalBuffer.Info.bmiHeader.biPlanes = 1;
 	// 8 bits each for Red, Green, Blue and 8 extra padded for alignment on 4B boundaries 
 	GlobalBuffer.Info.bmiHeader.biBitCount = 32;
@@ -116,8 +138,13 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine,
 	WindowClass.hInstance = Instance;
 	// WindowClass.hIcon
 	WindowClass.lpszClassName = "Program";
+	win32_offscreen_buffer b = {};
+	b.Memory = VirtualAlloc(0, RENDER_WIDTH * RENDER_HEIGHT * 4, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 	HotLoadBuffer(&GlobalBuffer);
-	//RenderWeirdGradient(&GlobalBuffer, 0, 0);
+	//RenderWeirdGradient(&GlobalBuffer, 32, 42);
+	LARGE_INTEGER LastCounter;
+	QueryPerformanceCounter(&LastCounter);
+	uint64 LastCycleCount = __rdtsc();
   if (RegisterClass(&WindowClass))
   {
     HWND Window = 
@@ -129,8 +156,8 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine,
 				//WS_OVERLAPPEDWINDOW | WS_VISIBLE,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
         0,
         0,
         Instance,
@@ -156,13 +183,35 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine,
           DispatchMessage (&Message);
         }
 
-       win32_window_dimension Dimension = Win32GetWindowDimension(Window);
-			 if (ShouldRender)
-			 {
-					Win32CopyBufferToWindow(
-						DeviceContext, Dimension.Width, Dimension.Height,
-						&GlobalBuffer);	
-			 }
+				win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+				if (ShouldRender)
+				{
+					// render just once in a loop since i just want to display static image and not burn my GPU
+					ShouldRender = false; 
+						Win32CopyBufferToWindow(
+							DeviceContext, Dimension.Width, Dimension.Height,
+							&GlobalBuffer);	
+				}
+
+
+				LARGE_INTEGER EndCounter;
+				uint64 EndCycleCount = __rdtsc();
+				QueryPerformanceCounter(&EndCounter);
+
+			 	int64 CounterElapsed = EndCounter.QuadPart - LastCounter.QuadPart;
+				real32 MSPerFrame = (real32)(((1000.0f*(real32)CounterElapsed) / (real32)PerfCounterFreq));
+				//int32 d = (CounterElapsed * (1/PerfCounterFreq));
+				real32 FPS = (real32)PerfCounterFreq / (real32)CounterElapsed;
+				uint64 CyclesElapsed = EndCycleCount - LastCycleCount;
+				// Mega cycles per second - so whatever value here we executed MCPF * 1000 * 1000 instructions
+				// this is just for easier viewing 
+				real32 MCPF = (real32)CyclesElapsed / (1000.0f * 1000.0f);
+				char Buffer[256];
+				
+				//sprintf(Buffer, "Milliseconds/frame: %fms FPS: %f Cycles: %f \n", MSPerFrame, FPS, MCPF);
+				//OutputDebugString(Buffer);
+				LastCounter = EndCounter;
+				LastCycleCount = EndCycleCount;
       }
     }
   }
@@ -199,7 +248,6 @@ void Observe()
     else
     {
       // no need to worry about duplicates because it will always be one file in directory we are looking at
-      OutputDebugString("CHANGE");
 			HotLoadBuffer(&GlobalBuffer);
     }  
 
@@ -213,18 +261,20 @@ void Win32CopyBufferToWindow(
 	// TODO: Fix aspect ratio
 	// Pretty much copy rectangle from our buffer to the screen
 	// That is hwy source and dest coords are the same
-	StretchDIBits(
+	int result = StretchDIBits(
 		DeviceContext,
 		/*
 		X,Y,Width,Height,
 		X,Y,Width,Height,
 		*/
 		0, 0, WindowWidth, WindowHeight,
-		0, 0, WIDTH, HEIGHT,
+		0, 0, RENDER_WIDTH, RENDER_HEIGHT,
 		Buffer->Memory,
 		&Buffer->Info,
 		DIB_RGB_COLORS,
 		SRCCOPY);
+	if (result == 0)
+		printf("Error in STRECHDIBBITS \n", result);
 }
 
 void RenderWeirdGradient(win32_offscreen_buffer *Buffer, int BlueOffset, int GreenOffset)
@@ -232,11 +282,11 @@ void RenderWeirdGradient(win32_offscreen_buffer *Buffer, int BlueOffset, int Gre
 	// TODO lets see what o ptimized does
 	// byte array pretty much
 	uint8 *Row = (uint8 *)Buffer->Memory;
-	for (int Y = 0; Y < HEIGHT; ++Y)
+	for (int Y = 0; Y < RENDER_HEIGHT; ++Y)
 	{
 		// uint8 *Pixel  = (uint8 *)Row;
 		uint32 *Pixel = (uint32 *)Row;
-		for (int X = 0; X < WIDTH; ++X)
+		for (int X = 0; X < RENDER_WIDTH; ++X)
 		{
 			/* 8 - bit red 8 bits of green 8 bits of blue and 8 bits of padding
 			Pixel in memory: RR GG BB xx
@@ -278,31 +328,35 @@ void RenderWeirdGradient(win32_offscreen_buffer *Buffer, int BlueOffset, int Gre
 		}
 
 		// Pointer arithimic, pretty much moving pointer to next row (memory is 1D but we think of it as 2D since its bitmap)
-		Row += (WIDTH * 4);
+		Row += (RENDER_WIDTH * 4);
 	}
 	
 }
 void HotLoadBuffer(win32_offscreen_buffer *Buffer)
 {
-	ShouldRender = false;
-	FILE* file = fopen(filePath, "r");
-	if (file == NULL)
+	
+
+	ifstream file(filePath, ios::binary | ios::ate);
+
+	if (!file.is_open())
 	{
 		OutputDebugStringA("Unable to open file");
 		return;
 	}
-	// just in case
-	fseek(file, 0, SEEK_END);
-	int size = ftell(file);
-	fseek(file, 0, SEEK_SET);
-	if (size != HEIGHT * WIDTH * 4)
+	int size = file.tellg();
+	file.seekg(0, ios::beg);
+	int ourBufferLen = RENDER_HEIGHT * RENDER_WIDTH * 4;
+
+
+	if (size < ourBufferLen)
 	{
-		OutputDebugStringA("WrongSizeOfFile");
+		char buff[200];
+		sprintf(buff, "Wrong size. Size: %d\n", size);
+		OutputDebugStringA(buff);
 		return;
 	}
-
-	fread(Buffer->Memory, 1, size, file);
-	fclose(file);
+	ShouldRender = false;
+	file.read((char*)Buffer->Memory, ourBufferLen);
 	ShouldRender = true;
 }
 win32_window_dimension Win32GetWindowDimension(HWND Window)
