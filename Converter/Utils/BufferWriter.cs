35536a83ab01cf3edad078e1d02f2ebf2fcf4d6a
WickedyWick <crvenialeksa@gmail.com>
2026-02-12 12:09:26 +0100

    Add static bufferwrite rhelper

---
+++ Converter/Utils/BufferWriter.cs
@@ -0,0 +1,59 @@
+﻿using System.Buffers.Binary;
+
+namespace Converter.Utils
+{
+  public static class BufferWriter
+  {
+    #region LittleEndian
+    public static void WriteInt32LE(ref Span<byte> buffer, ref int pos, int value)
+    {
+      BinaryPrimitives.WriteInt32LittleEndian(buffer.Slice(pos, 4), value);
+      pos += 4;
+    }
+
+    public static void WriteUInt32LE(ref Span<byte> buffer, ref int pos, uint value)
+    {
+      BinaryPrimitives.WriteUInt32LittleEndian(buffer.Slice(pos, 4), value);
+      pos += 4;
+    }
+
+    public static void WriteInt16LE(ref Span<byte> buffer, ref int pos, short value)
+    {
+      BinaryPrimitives.WriteInt16LittleEndian(buffer.Slice(pos, 2), value);
+      pos += 2;
+    }
+
+    public static void WriteUInt16LE(ref Span<byte> buffer, ref int pos, ushort value)
+    {
+      BinaryPrimitives.WriteUInt16LittleEndian(buffer.Slice(pos, 2), value);
+      pos += 2;
+    }
+    #endregion LittleEndian
+
+    #region BigEndian
+    public static void WriteInt32IntBE(ref Span<byte> buffer, ref int pos, int value)
+    {
+      BinaryPrimitives.WriteInt32BigEndian(buffer.Slice(pos, 4), value);
+      pos += 4;
+    }
+
+    public static void WriteUInt32BE(ref Span<byte> buffer, ref int pos, uint value)
+    {
+      BinaryPrimitives.WriteUInt32BigEndian(buffer.Slice(pos, 4), value);
+      pos += 4;
+    }
+
+    public static void WriteInt16BE(ref Span<byte> buffer, ref int pos, short value)
+    {
+      BinaryPrimitives.WriteInt16BigEndian(buffer.Slice(pos, 2), value);
+      pos += 2;
+    }
+
+    public static void WriteUInt16BE(ref Span<byte> buffer, ref int pos, ushort value)
+    {
+      BinaryPrimitives.WriteUInt16BigEndian(buffer.Slice(pos, 2), value);
+      pos += 2;
+    }
+    #endregion BigEndian
+  }
+}
