dccd5270c0f6f6f4092613eaefcaf9089bd02f69
WickedyWick <crvenialeksa@gmail.com>
2026-02-16 14:20:52 +0100

    Add ReadOnlySpan APIs to BufferReader to avoid casting

--- Converter/Utils/BufferReader.cs
+++ Converter/Utils/BufferReader.cs
@@ -49,10 +49,38 @@ namespace Converter.Utils
       pos += 2;
       return num;
     }
+
+    public static int ReadInt32LE(ref ReadOnlySpan<byte> buffer, ref int pos)
+    {
+      int num = BinaryPrimitives.ReadInt32LittleEndian(buffer.Slice(pos, 4));
+      pos += 4;
+      return num;
+    }
+
+    public static uint ReadUInt32LE(ref ReadOnlySpan<byte> buffer, ref int pos)
+    {
+      uint num = BinaryPrimitives.ReadUInt32LittleEndian(buffer.Slice(pos, 4));
+      pos += 4;
+      return num;
+    }
+
+    public static short ReadInt16LE(ref ReadOnlySpan<byte> buffer, ref int pos)
+    {
+      short num = BinaryPrimitives.ReadInt16LittleEndian(buffer.Slice(pos, 2));
+      pos += 2;
+      return num;
+    }
+
+    public static ushort ReadUInt16LE(ref ReadOnlySpan<byte> buffer, ref int pos)
+    {
+      ushort num = BinaryPrimitives.ReadUInt16LittleEndian(buffer.Slice(pos, 2));
+      pos += 2;
+      return num;
+    }
     #endregion LittleEndian
 
     #region BigEndian
-    public static int ReadInt32IntBE(ref Span<byte> buffer, ref int pos)
+    public static int ReadInt32BE(ref Span<byte> buffer, ref int pos)
     {
       int num = BinaryPrimitives.ReadInt32BigEndian(buffer.Slice(pos, 4));
       pos += 4;
@@ -79,6 +107,35 @@ namespace Converter.Utils
       pos += 2;
       return num;
     }
+
+    public static int ReadInt32BE(ref ReadOnlySpan<byte> buffer, ref int pos)
+    {
+      int num = BinaryPrimitives.ReadInt32BigEndian(buffer.Slice(pos, 4));
+      pos += 4;
+      return num;
+    }
+
+    public static uint ReadUInt32BE(ref ReadOnlySpan<byte> buffer, ref int pos)
+    {
+      uint num = BinaryPrimitives.ReadUInt32BigEndian(buffer.Slice(pos, 4));
+      pos += 4;
+      return num;
+    }
+
+    public static short ReadInt16BE(ref ReadOnlySpan<byte> buffer, ref int pos)
+    {
+      short num = BinaryPrimitives.ReadInt16BigEndian(buffer.Slice(pos, 2));
+      pos += 2;
+      return num;
+    }
+
+    public static ushort ReadUInt16BE(ref ReadOnlySpan<byte> buffer, ref int pos)
+    {
+      ushort num = BinaryPrimitives.ReadUInt16BigEndian(buffer.Slice(pos, 2));
+      pos += 2;
+      return num;
+    }
+
     #endregion BigEndian
   }
 }
